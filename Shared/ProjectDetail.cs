84f7d92e281b705b893aa82fd7b517e251253c75
Brandon Snyder <brandonsnyder@truckstop.com>
2022-05-25 11:49:25 -0600

    templated project page and starting on colors

---
+++ Shared/ProjectDetail.cs
@@ -0,0 +1,16 @@
+﻿namespace Portfolio.Shared
+{
+    public class ProjectDetail
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string? ViewPath { get; set; }
+
+        public ProjectDetail(string title, string description, string? viewPath = null)
+        {
+            Title = title;
+            Description = description;
+            ViewPath = viewPath;
+        }
+    }
+}
